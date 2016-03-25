d6f2d3b0f05de3e3443805006aa38efc5fff2224
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-03-25 14:28:29 -0400

    Update bundle & vendor bundle

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(21);
+	module.exports = __webpack_require__(49);
 
 
 /***/ },
@@ -9943,7 +9943,7 @@
 
 /***/ },
 
-/***/ 21:
+/***/ 49:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
