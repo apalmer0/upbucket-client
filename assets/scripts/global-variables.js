ed207e1b3132297cc3b7cf09c300084034b19d03
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-03-25 14:38:23 -0400

    Update baseUrl ternary for deployed app

--- assets/scripts/global-variables.js
+++ assets/scripts/global-variables.js
@@ -3,5 +3,5 @@
 module.exports = {
   baseUrl: document.location.hostname === 'localhost' ?
     'http://localhost:3000' :
-    'http://localhost:3000',
+    'https://pacific-chamber-64011.herokuapp.com',
 };
